76f8a2783aa7324230a2bbdbbb22ded93a4795e6
zzp <zhang9zp970@gmail.com>
2018-03-10 18:36:19 +0800

    finished basic parts. now can read from esp8266, but need to change the function to make it easier to use

---
+++ main.c
@@ -0,0 +1,92 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 7, 2018
+ *      Author: zzp
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+void ConfigureUART0(void)
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE,GPIO_PIN_0|GPIO_PIN_1);
+    UARTClockSourceSet(UART0_BASE,UART_CLOCK_PIOSC);
+    UARTStdioConfig(0,115200,16000000);
+}
+
+void UARTIntHandler(void)
+{
+    uint32_t ui32Status;
+    char words[100]={0};
+    int i=0;
+    ui32Status = UARTIntStatus(UART1_BASE, true);
+    UARTIntClear(UART1_BASE, ui32Status);
+    while(UARTCharsAvail(UART1_BASE)&&i<100)
+    {
+        words[i]=UARTCharGetNonBlocking(UART1_BASE);
+        i++;
+
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+    }
+    words[i+1]='\0';
+    UARTprintf("%s\n",words);
+}
+
+UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ui32Count--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        UARTCharPutNonBlocking(UART1_BASE, *pui8Buffer++);
+    }
+}
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);//40mhz
+    FPUEnable();
+    FPULazyStackingEnable();
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    IntMasterEnable();
+    GPIOPinConfigure(GPIO_PC4_U1RX);
+    GPIOPinConfigure(GPIO_PC5_U1TX);
+    GPIOPinTypeUART(GPIO_PORTC_BASE,GPIO_PIN_4|GPIO_PIN_5);
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8|UART_CONFIG_STOP_ONE|UART_CONFIG_PAR_NONE));
+    IntEnable(INT_UART1);
+    UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
+    ConfigureUART0();
+    UARTprintf("hello world ! \n");
+    UARTSend((uint8_t *)"AT\r\n",5);
+    while(1)
+    {
+
+    }
+}
+
+
+
