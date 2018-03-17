ab6514762f1945f3dcb8c4343ae4a1cdfc6082a8
zzp <zhang9zp970@gmail.com>
2018-03-17 16:22:00 +0800

    wifi communication with esp8266 success

--- main.c
+++ main.c
@@ -34,15 +34,31 @@ void UARTIntHandler(void)
 {
     uint32_t ui32Status;
     char words[100]={0};
-    int i=0;
+    char Commands[100]={0};
+    int i=0,j=0,k=0,m=0,n=0;
     ui32Status = UARTIntStatus(UART1_BASE, true);
     UARTIntClear(UART1_BASE, ui32Status);
     while(UARTCharsAvail(UART1_BASE)&&i<100)
     {
         words[i]=UARTCharGetNonBlocking(UART1_BASE);
+        if(words[i]=='$')
+        {
+            j=i;
+        }
         i++;
     }
     words[i+1]='\0';
+    m=i+1;
+    if(j!=0)
+    {
+        for (k=j;k<m;k++)
+        {
+            Commands[n]=words[k];
+            n++;
+        }
+        Commands[n+1]='\0';
+        UARTprintf("%s",Commands);
+    }
     UARTprintf("%s",words);
 }
 
@@ -51,14 +67,13 @@ void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
     while(ui32Count--)
     {
         UARTCharPutNonBlocking(UART1_BASE, *pui8Buffer++);
+        SysCtlDelay(SysCtlClockGet()/300);//make sure UART work smoothly,delay some time
     }
 }
 
-//void
-
 int main(void)
 {
-    char* string="AT+GMR\r\n";
+    char* string="AT\r\n";
 
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);//40mhz
     FPUEnable();
@@ -76,7 +91,10 @@ int main(void)
     UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
     ConfigureUART0();
     UARTprintf("hello world ! \n");
-    //string="AT\r\n";
+    string="AT+CIPMUX=1\r\n";
+    UARTSend((uint8_t *)(string),strlen(string));
+    //SysCtlDelay(SysCtlClockGet()/30);//make sure the output is correct,delay some time
+    string="AT+CIPSERVER=1,6666\r\n";
     UARTSend((uint8_t *)(string),strlen(string));
     while(1)
     {
