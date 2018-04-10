acc900cf6edcbaaa9f8668cfa0b0ebf1faa75765
zzp <zhang9zp970@gmail.com>
2018-04-10 15:29:04 +0800

    read and send success with wifi-uart module

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
  *  Created on: Mar 7, 2018
  *      Author: zzp
  */
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -20,7 +19,6 @@
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
 char Commands[100]={0};
-char channel='0';
 void ConfigureUART0(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -34,43 +32,18 @@ void ConfigureUART0(void)
 
 void UARTIntHandler(void)
 {
-    char get='0';
     uint32_t ui32Status;
     char words[100]={0};
-    int i;
+    int i=0;
     ui32Status = UARTIntStatus(UART1_BASE, true);
     UARTIntClear(UART1_BASE, ui32Status);
-    while(UARTCharsAvail(UART1_BASE))
+    while(UARTCharsAvail(UART1_BASE)&&i<100)
     {
-        get=UARTCharGetNonBlocking(UART1_BASE);
-        /*if(get=='$')
-        {
-            i=0;
-            get=UARTCharGetNonBlocking(UART1_BASE);
-            while(get!='#')
-            {
-                words[i]=get;
-                get=UARTCharGetNonBlocking(UART1_BASE);
-                i++;
-            }
-            words[i+1]='\0';
-            //UARTprintf("%s",words);
-            strcpy(Commands,words);
-        }
-        else
-        {*/
-            UARTprintf("%c",get);
-            /*if(get=='+')
-            {
-                UARTCharGetNonBlocking(UART1_BASE);
-                UARTCharGetNonBlocking(UART1_BASE);
-                UARTCharGetNonBlocking(UART1_BASE);
-                UARTCharGetNonBlocking(UART1_BASE);
-                channel=UARTCharGetNonBlocking(UART1_BASE);
-            }*/
-
-        //}
+        words[i]=UARTCharGetNonBlocking(UART1_BASE);
+        i++;
     }
+    words[i+1]='\0';
+    UARTprintf("%s",words);
 }
 
 void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
@@ -78,21 +51,12 @@ void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
     while(ui32Count--)
     {
         UARTCharPutNonBlocking(UART1_BASE, *pui8Buffer++);
-        SysCtlDelay(SysCtlClockGet()/300);//make sure UART work smoothly,delay some time
     }
-}
-
-void WIFISend(void)
-{
-    char* strings="AT+CIPSEND=0,12\r\n";
-    UARTSend((uint8_t *)(strings),strlen(strings));
-    strings="test";
-    UARTSend((uint8_t *)(strings),strlen(strings));
 }
 
 int main(void)
 {
-    char* string="AT\r\n";
+    char* string="zzpw2\r\n";
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);//40mhz
     FPUEnable();
     FPULazyStackingEnable();
@@ -109,22 +73,13 @@ int main(void)
     UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
     ConfigureUART0();
     UARTprintf("hello world ! \n");
-    string="AT+CIPMUX=1\r\n";
-    UARTSend((uint8_t *)(string),strlen(string));
-    string="AT+CIPSERVER=1,6666\r\n";
-    UARTSend((uint8_t *)(string),strlen(string));
-    string="AT+CIPSEND=0,12\r\n";
-    UARTSend((uint8_t *)(string),strlen(string));
-    string="test\r\n";
-    UARTSend((uint8_t *)(string),strlen(string));
     while(1)
     {
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
         SysCtlDelay(SysCtlClockGet() / 6);
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
         SysCtlDelay(SysCtlClockGet() /6);
-        //UARTprintf("%c",channel);
-        //WIFISend();
+        UARTSend((uint8_t *)(string),strlen(string));
     }
 }
 
